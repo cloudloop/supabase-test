5b59781e8a360bd23c85e1cee987b549f2703147
Cloudloop <axel.k.ingo@gmail.com>
2022-10-31 15:10:31 +0100

    working on local env var

--- supamain.py
+++ supamain.py
@@ -1,9 +1,8 @@
 import os
 from supabase import create_client, Client
-from dotenv import load_dotenv
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+url: str = os.environ.get("SPB_URL")
+key: str = os.environ.get("SPB_KEY")
 
 print(url,key)
 #supabase: Client = create_client(url, key)
